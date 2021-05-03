384c87445ce172f6a6af0a1bbc433111848362d5
A-Little-Hat <soumyasarkar309@gmail.com>
2021-05-03 21:47:23 +0530

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Resume from './Resume';
 
 const Home = () => {
     return (
         <div>
             home page
+            <Resume/>
         </div>
     );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            text editor
+        </div>
+    );
+}
+
+export default Resume;
+
+
