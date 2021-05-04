e148760cea975300fcb88088a1c41dd1ed1ffc53
A-Little-Hat <soumyasarkar309@gmail.com>
2021-05-04 19:51:41 +0530

    print.js updated

--- src/components/Print.js
+++ src/components/Print.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+
+import '../css/print.css'
 
 const Print = (props) => {
+    var result=props.data
+    console.log(props.data)
+    var name=result.name
+    var profile=result.profile
+    var workXP=result.workXP
+    var education=result.education
+    window.onload =()=>{
+        document.getElementById("profile").innerHTML=profile
+        }
+        useEffect(() => {
+            document.getElementById("profile").innerHTML=profile
+        }, [profile])
+    
     return (
-        <div>
-            {
-                console.log(props.data)
-            }
+        
+        <div className="row m-4 mt-5">
+            <div className="col-md-4">
+                {name}
+            </div>
+            <div className="col-md-8">
+                <button className=" MyHeading " >Profile</button>
+                <div id="profile">
+                    
+                </div>
+
+            </div>
         </div>
+        
     );
+    
 }
 
 export default Print;
